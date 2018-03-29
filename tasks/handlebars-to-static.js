893b940d474f2088cc790a948e569112dd0b77c7
Chris McClellan <chris@chrismcclellan.com>
2018-03-29 13:38:15 +0800

    nailed down all partial building and putting built pages in root

--- tasks/handlebars-to-static.js
+++ tasks/handlebars-to-static.js
@@ -5,16 +5,18 @@
  *   For usage docs see: https://github.com/gruntjs/grunt-contrib-handlebars
  *
  */
-var _       = require('lodash');
-var data    = require('../data');
+var _ = require('lodash');
+var data = require('../data');
 var helpers = require('../src/templates/helpers');
 
 module.exports = function(g) {
 
-    var paths     = getTemplatePaths(g);
-    var contents  = getTemplateContents(g, paths);
+    var dest = g.option('dest');
+
+    var paths = getTemplatePaths(g);
+    var contents = getTemplateContents(g, paths);
     var templates = _.zipObject(paths, contents);
-    var grouped   = groupTemplates(templates);
+    var grouped = groupTemplates(templates);
 
     g.config.set('handlebars_to_static', {
 
@@ -30,69 +32,63 @@ module.exports = function(g) {
 
                 file_context: function(src, dest, ctx) {
 
-                    // console.log('file_context', src, dest, ctx);
-
-                    var key = src.replace(/\.hbs$/, '');
-
-                    if (key.indexOf('pages/') > -1) 
-                        key = key.split('pages/')[1];
+                    var key = src.replace(/\/page\.hbs$/, '').replace(/\.hbs$/, '');
+                    if (key.indexOf('pages/') > -1) key = key.split('pages/')[1];
 
                     var body = _.get(grouped.pages, key);
                     var partials = _.set(grouped.partials, 'body', body);
 
-                    console.log('partials');
-                    console.log(partials);
-
-                    console.log('exists', g.file.exists('./src/templates/layouts/default.hbs'));
-
                     return {
                         src: './src/templates/layouts/default.hbs',
-                        partials: partials
+                        partials: partials,
+                        dest: key + '.html'
                     };
                 }
             },
-
+            expand: true,
             cwd: 'src/templates/pages/',
-            src: ['*.hbs'],
-            dest: './'
+            src: ['**/*.hbs'],
+            dest: 'build/pages/'
         }
     });
 
     g.loadNpmTasks('grunt-handlebars-to-static');
 };
 
-function getTemplatePaths(grunt) {
-    return grunt.file.expand(['src/templates/**/*.hbs'], {
+function getTemplatePaths(g) {
+    return g.file.expand(['src/templates/**/*.hbs'], {
         cwd: './src/templates',
         filter: 'isFile'
     });
 }
 
-function getTemplateContents(grunt, paths) {
-    return _.map(paths, grunt.file.read);
+function getTemplateContents(g, paths) {
+    return _.map(paths, g.file.read);
 }
 
 function cleanPaths(templates) {
-    return _.zipObject(_.map(templates, function (_path) {
-        return _path.replace('.hbs', '').replace('src/templates/', '');
+    return _.zipObject(_.map(templates, function(_p) {
+        return _p.replace('/page.hbs', '').replace('.hbs', '').replace('src/templates/', '');
     }), contents);
 }
 
 function groupTemplates(templates) {
 
     var o = {};
-        o.pages = {};
-        o.partials = {};
-        o.layouts = {};
+    o.pages = {};
+    o.partials = {};
+    o.layouts = {};
 
     _.each(templates, function(val, key) {
 
-        var match = key.match(/.*\/templates\/(partials|layouts|pages)\/.*/)[1];
+        var islayout  = key.indexOf('layouts/') > -1;
+        var ispage    = key.indexOf('pages/') > -1;
+        var ispartial = key.indexOf('partials/') > -1;
 
-        if (!_.has(o, match))
-            return _.set(o, ['unmatched', key], val);
+        if (!islayout && !ispage && !ispartial) throw "Does not match any of " + _.join(_.keys(o), ", ");
 
-        var cleankey = key.split(match)[1].replace(/(\.hbs)$/, '').replace(/^(\/)/, '');
+        var match = islayout ? 'layouts' : ispartial ? 'partials' : 'pages';
+        var cleankey = key.split(match)[1].replace(/(\/page\.hbs)$/, '').replace(/(\.hbs)$/, '').replace(/^(\/)/, '');
 
         _.set(o, [match, cleankey], val);
     });
@@ -100,14 +96,15 @@ function groupTemplates(templates) {
     return o;
 }
 
-function composePartials(paths, grunt) {
+function composePartials(paths, g) {
     var keys = _.map(paths, sanitizePartials);
-    var vals = _.map(paths, grunt.file.read);
+    var vals = _.map(paths, g.file.read);
     return _.zipObject(keys, vals);
 }
 
 function sanitizePartials(path) {
-    return path.replace('.hbs', '')
+    return path.replace('/page.hbs', '')
+        .replace('.hbs', '')
         .replace('src/templates/', '')
         .replace('pages/', '')
         .replace('partials/', '');
