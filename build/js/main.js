a29dc2b0ad13b54de84df0cf2fff86ba6f083915
Chris McClellan <chris@chrismcclellan.com>
2018-04-24 12:36:51 +0800

    removing scroll animation stuff

--- build/js/main.js
+++ build/js/main.js
@@ -18371,19 +18371,9 @@ module.exports = function canopy() {
     var $canopy = $('#canopy');
     var $objective = $('.objective', $canopy);
 
-    $win.on('scroll.canopy', function() {
-        $objective.removeClass('init');
-
-    //     var scrolled   = $doc.scrollTop();
-    //     var threshold  = $win.height();
-    //     var percentage = _.clamp(percent(threshold).is(scrolled), 0, 100);
-
-    //     var top = percentage + 50;
-
-    //     $canopy.css({
-    //         transform: 'translate(0%, -' + top +'%)'
-    //     });
-    });
+    // window.setTimeout(function() {
+    //     $objective.addClass('flash');
+    // }, 1200);
 }
 
 
